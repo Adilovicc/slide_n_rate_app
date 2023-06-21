b3666f2c51459d9e18018e4a2da14637702cf107
Adilovicc <adiloviccali@gmail.com>
2023-06-22 01:19:42 +0200

    Oopet

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
     images: {
         domains: ["firebasestorage.googleapis.com"],
     },
- 
-    webpack: (
-          config,
-          { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-          // Important: return the modified config
-          return config
-    },
-      
 }
 
 module.exports = nextConfig
