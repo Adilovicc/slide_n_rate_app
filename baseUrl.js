c9c906c16e3f58db446209de5bf4294b43e9329b
Adilovicc <adiloviccali@gmail.com>
2023-06-23 21:02:53 +0200

    jjj

--- baseUrl.js
+++ baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl =  'https://slide-n-rate-project.vercel.app/';
+const baseUrl =  'https://slide-n-rate-app.vercel.app/';
 //const baseUrl =  'http://localhost:3000/';
 
 export {baseUrl};
